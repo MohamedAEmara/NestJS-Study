f2822429e4e8a07723e99d9b27ae264d79b6a418
Mohamed Emara <medoemara104@gmail.com>
2024-03-13 02:23:03 +0200

    use module imports & exports

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { CarsController } from "./cars.controller";
 import { CarsService } from "./cats.service";
+import { CommonModule } from "src/common/common.module";
 
 @Module({
     controllers: [CarsController],
     providers: [CarsService],
+    // If you want to make "CarsService" shared between other modules
+    // You have to add it to export array like this:
+    imports: [CommonModule],
+    exports: [CarsService, CommonModule]
+    // Now any module that imports the (CarsModule) has access to the "CarsService".
+
+    // Note: A module can also export a module it already imports ass seen ABOVE...
 })
 
 export class CarsModule {}
---
+++ src/common/common.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+
+
+@Module({
+   // Empty for now...  
+})
+
+export class CommonModule {}
