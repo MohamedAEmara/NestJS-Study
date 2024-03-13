41ae8af9c271561adb165994d45c31611aef4cc7
Mohamed Emara <medoemara104@gmail.com>
2024-03-13 03:16:17 +0200

    Practice different ways of using middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CarsController } from './cars/cars.controller';
 import * as express from 'express';
 import { CarsService } from './cars/cats.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { CarsLoggerMiddleware } from './common/middleware/carslogger.middleware';
 
 @Module({
   imports: [],
@@ -24,5 +25,20 @@ export class AppModule implements NestModule {
     // NOTE: you can also restrict the middleware to a particular request method like this:
     consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.GET });
     // Now, this middleware will be applied on all GET method with any route. 
+
+    consumer.apply(CarsLoggerMiddleware).forRoutes(CarsController);
+    // NOTE: this will be applied on all routes in (CarsController)...
+    // ANOTHER NOTE: forRoutes can take more than one argument...
+
+
+    // We can also execlude some routes from using a middleware 
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'cars', method: RequestMethod.POST },
+        { path: 'cars', method: RequestMethod.PATCH },
+        'cars/(.*)',
+      )
+      .forRoutes(CarsController);
   }
 }
